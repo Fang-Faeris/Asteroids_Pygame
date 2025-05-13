d6e3623af1911a82da7a424a8ee7a437d3cfa262
Fang-Faeris <joshuajensen18@gmail.com>
2025-05-12 19:37:16 -0500

    ROTATION BABY

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
+
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
@@ -25,4 +27,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
